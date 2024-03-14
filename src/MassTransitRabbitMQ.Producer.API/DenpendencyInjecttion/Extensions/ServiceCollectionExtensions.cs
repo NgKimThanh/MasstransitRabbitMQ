4a5e17cfb3659010102e044bd5dd2ae4dece12b9
nkthanh977 <thanh.nguyen@gosmartlog.com>
2024-03-14 22:54:22 +0700

    init

---
+++ src/MassTransitRabbitMQ.Producer.API/DenpendencyInjecttion/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,28 @@
+﻿using MassTransit;
+using MassTransitRabbitMQ.Producer.API.DenpendencyInjecttion.Options;
+
+namespace MassTransitRabbitMQ.Producer.API.DenpendencyInjecttion.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddConfigureMasstransitRabbitMQ(this IServiceCollection services, IConfiguration configuration)
+        {
+            var masstransitConfiguration = new MasstransitConfiguration();
+            configuration.GetSection(nameof(MasstransitConfiguration)).Bind(masstransitConfiguration);
+
+            services.AddMassTransit(mt =>
+            {
+                mt.UsingRabbitMq((context, bus) =>
+                {
+                    bus.Host(masstransitConfiguration.Host, masstransitConfiguration.VHost, c =>
+                    {
+                        c.Username(masstransitConfiguration.UserName);
+                        c.Password(masstransitConfiguration.Password);
+                    });
+                });
+            });
+
+            return services;
+        }
+    }
+}
